d9bde1d2b8e2055bb6a9cf8b6e6ffa6478a96695
mariopino <mariopino@protonmail.com>
2021-03-05 12:30:35 +0100

    update

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ const fetch = require('node-fetch');
 const { BigNumber } = require('bignumber.js');
 
 const query = `
-  query MyQuery {
-    event(where: {section: {_eq: "claims"}, method: {_eq: "Claimed"}}, order_by: {block_number: asc}) {
-      block_number
-      data
+  query account {
+    account(where: { free_balance: { _lt: 200000000000 } }, order_by: {free_balance: asc}) {
+      account_id
+      available_balance
+      free_balance
+      locked_balance
     }
   }`;
 
-fetch('https://kusama.polkastats.io/api/v3', {
+const nodeWs = 'https://subsocial.polkastats.io/api/v3';
+
+fetch(nodeWs, {
   method: 'POST',
   body: JSON.stringify({query}),
 }).then(res => res.text())
   .then(body => {
-    // console.log(JSON.stringify(JSON.parse(body).data.event, null, 2));
-    JSON.parse(body).data.event.forEach(event => {
-      const data = JSON.parse(event.data);
-      // console.log(`block: ${event.block_number}, kusama address: ${data[0]}, ethereum address: ${data[1]}, amount: ${data[2]}`);
-      console.log(`${event.block_number},${data[0]},${data[1]},${new BigNumber(data[2]).toString(10)}`);
+    JSON.parse(body).data.account.forEach(account => {
+      console.log(`${account.account_id},${account.available_balance},${account.free_balance},${account.locked_balance}`);
     });
     
   })
